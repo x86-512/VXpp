6cd1445c968cebf328fea8e90b8b1f9d6d3c55a5
x256_64 <70492320+x256-64@users.noreply.github.com>
2024-08-05 20:10:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,12 @@ import jpype
 
 #Remember to add XFG option
 
-os.system("export GHIDRA_INSTALL_DIR=/usr/share/ghidra")
-#GHIDRA_INSTALL_DIR = os.getenv("GHIDRA_INSTALL_DIR")
-
 common_vtable_registers:list[str] = ["ax", "cx", "dx", "bx"]
 
 jump_instructions:list[str] = ["jmp", "jne", "je", "jo", "jno", "js", "jns", "jz", "jnz", "jb", "jnae", "jnb", "jc", "jae", "jnc", "jbe", "jna", "ja", "jnbe", "jl", "jnge", "jge", "jnl", "jle", "jg", "jnle", "jp", "jpe", "jnp", "jpo", "jcxz", "jecxz"]
 
 #Look for a virtual function calling other virtual functions in a loop, get its location in the vtable and get the vtable address
 #Pass a disassembled ghidra function in the argument
-def find_mlg(max_len:int) -> None:
-    pass
-
-def get_disassembly(program, func) -> None:
-    if func is None:
-        return "None"
-    print(type(func))
-    func.getDisassembler()
-    breakpoint()
-    disassembler = program.getListing().getDisassembler(func.getBody())
-    print(disassembler.getInstructions(func.getBody()))
-
 
 def convert_to_str_list(instructions:list) -> list[str]:
     returnable:list[str] = []
@@ -57,14 +42,7 @@ def instruction_ind_not_reg(instructions:list[str], inst:str) -> int:
         if inst.lower() in i.lower() and "0x" in inst:
             return ind
     return -1
-    #4 Conditions:
-    #1. Call 
-    #2. Jmp target before call address,
-    #3. Check for use of conventional registers and instructions for vtable calls
-    #4. __guard_dispatch_icall_fptr call (If true, add usability level by 1)
-    #5. (Planned) Check if the gadget matches the parameter and return type of another function for XFG
-    #Disqualifiers: No ret, function too long, 
-    #If CFG is not met, the confidence level goes down
+    
 def is_mlg(instructions:list, addr_set) -> [bool, int]:
     #Find the VTABLE where the start of the function base is mentioned
     instructions_readable = convert_to_str_list(instructions)
@@ -83,6 +61,7 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
     call_regs:list = []
     vtable_indexes = []
     call_indexes = []
+    
     for ind, instr in enumerate(instructions_readable):
         start_ind = instr.find("[")
         end_ind = instr.find("]")
@@ -98,6 +77,7 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
             if deref_markers==2:
                 modified_regs.append(instr.split(" ")[1].split(",")[0].lower())
                 vtable_indexes.append(ind)
+                
     for i in vtable_indexes:
         for ind, instr in enumerate(instructions_readable[i:], start=i):
             start_ind = instr.find("[")
@@ -120,10 +100,8 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
             if i.split(" ")[0].lower()==j:
                 if int(i.split(" ")[1], 16)>=int("0x"+str(addr_set).split(',')[0][2:], 16) and int(i.split(" ")[1], 16)<=int("0x"+str(addr_set).split(' ')[1][1:-1], 16):
                     conditionals[1] = True
-
+                    
     return [True if conditionals[0] and conditionals[1] else False, 0] #usability not added yet
-
-
 
 def test_ghidra():
     try:
@@ -138,22 +116,6 @@ def test_ghidra():
     except ValueError:
         print("Please set your GHIDRA_INSTALL_DIR environment variable to your ghidra installation directory\nHint: If you are on Linux, run \'which ghidra\' to find the installation directrory")
         exit()
-    #Get the address of the VTABLE functions, then get the address of the call instruction,
-    #Get the function start address
-
-    #Now check for a jmp
-
-            #Now check for a dereference in a future instruction
-            #Check for proper dereferences
-            #Add a normal register and a multiply arch term that need to be found
-    #call_ind:int = instruction_index(instructions_readable, 'CALL')
-    #print(call_ind)
-    #print(instructions_readable)
-    #Check if register's move instruction has [r1 + arch*r2], if so, check if that register is dereferenced before a call instruction, check if the register that is dereferencing it is being called
-    #So Search for get inside the [] and see if it has 1 register + another register*arch, don't check what it is, only check what it is being moved to, then check if that is being dereferenced just with [], track all dereferences of it to see if the deref is called
-    #
-    #If there is a jump instruction, if it is > than the function base and < than the call, continue
-
 
 def main() -> None:
     test_ghidra()
@@ -166,17 +128,8 @@ def main() -> None:
         while iterator.hasNext():
             func = iterator.next()
             instructions:list[str] = list(program.getListing().getInstructions(func.getBody(), 1))
-            #print(func)
-            #print(func.getEntryPoint()) #Search the data section for this
-            #print(type(func.getEntryPoint()))
-            #print(program.getListing().getInstructionAt(func.getEntryPoint())) #Need address range, also only gets code
-            #print(f"FUNC: {func.getBody()}")
-            #print(instructions) #Need address range, also only gets code
             if (is_mlg(instructions, func.getBody())[0]):
                 print(f"Main Loop Gadget found at: {func.getEntryPoint()}")
-            #print(program.getListing().getCodeUnits(func.getBody())) #Need address range, also only gets code
-            #print(get_disassembly(program, func))
-        #print(f"PROGRAM: {program}")
 
 if __name__=="__main__":
     main()
