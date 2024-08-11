ac9a96b5dc0f14f194142d069cd462acf7bd6c47
x256_64 <70492320+x256-64@users.noreply.github.com>
2024-08-11 11:59:39 -0400

    Updated the jump conditional
    The script now checks for the address of the call and the address of the jump. If the call address is before the jump address, but after or equal to the jump target, the function is marked.

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os
 import jpype
 
 #Remember to add XFG option
+#To bypass xfg, find a virtual function with the same hash
+
+#Doesn't work
+os.system("export GHIDRA_INSTALL_DIR=/usr/share/ghidra")
+#GHIDRA_INSTALL_DIR = os.getenv("GHIDRA_INSTALL_DIR")
 
 common_vtable_registers:list[str] = ["ax", "cx", "dx", "bx"]
 
@@ -11,6 +16,17 @@ jump_instructions:list[str] = ["jmp", "jne", "je", "jo", "jno", "js", "jns", "jz
 
 #Look for a virtual function calling other virtual functions in a loop, get its location in the vtable and get the vtable address
 #Pass a disassembled ghidra function in the argument
+def find_mlg(max_len:int) -> None:
+    pass
+
+def get_disassembly(program, func) -> None:
+    if func is None:
+        return "None"
+    print(type(func))
+    func.getDisassembler()
+    breakpoint()
+    disassembler = program.getListing().getDisassembler(func.getBody())
+    print(disassembler.getInstructions(func.getBody()))
 
 def convert_to_str_list(instructions:list) -> list[str]:
     returnable:list[str] = []
@@ -42,7 +58,14 @@ def instruction_ind_not_reg(instructions:list[str], inst:str) -> int:
         if inst.lower() in i.lower() and "0x" in inst:
             return ind
     return -1
-    
+    #4 Conditions:
+    #1. Call 
+    #2. Jmp target before call address,
+    #3. Check for use of conventional registers and instructions for vtable calls
+    #4. __guard_dispatch_icall_fptr call (If true, add usability level by 1)
+    #5. (Planned) Check if the gadget matches the parameter and return type of another function for XFG
+    #Disqualifiers: No ret, function too long, 
+    #If CFG is not met, the confidence level goes down
 def is_mlg(instructions:list, addr_set) -> [bool, int]:
     #Find the VTABLE where the start of the function base is mentioned
     instructions_readable = convert_to_str_list(instructions)
@@ -54,14 +77,13 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
     #1: Is there a VTABLE call?
     #2: Is the jump after the call and does it go before the call, is it within the function?
     #3 (Bonus): Is the call protected by CFG?
-    conditionals:list[bool] = [False, False]
+    conditionals:list[bool] = [False, False, False]
     usability:int = 0
 
     modified_regs:list = []
     call_regs:list = []
     vtable_indexes = []
     call_indexes = []
-    
     for ind, instr in enumerate(instructions_readable):
         start_ind = instr.find("[")
         end_ind = instr.find("]")
@@ -77,7 +99,6 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
             if deref_markers==2:
                 modified_regs.append(instr.split(" ")[1].split(",")[0].lower())
                 vtable_indexes.append(ind)
-                
     for i in vtable_indexes:
         for ind, instr in enumerate(instructions_readable[i:], start=i):
             start_ind = instr.find("[")
@@ -87,23 +108,43 @@ def is_mlg(instructions:list, addr_set) -> [bool, int]:
                     if len(instr[start_ind+1:end_ind])==3 and instr[start_ind+1:end_ind].lower()==modified_reg:
                         call_regs.append(instr.split(" ")[1].split(",")[0])
                         call_indexes.append(ind)
-
+    
+    call_addr = 0
     for i in call_indexes: #what about guard check?
         for ind, instr in enumerate(instructions_readable[i:], start=i):
             if len(instr.split(" ")) >0 and instr.split(" ")[0].lower()=="call":
                 for reg in call_regs:
                     if instr.split(" ")[1]==reg:
                         conditionals[0] = True
-
-    for i in instructions_readable:
+                        call_addr = int(str(instructions[ind].getAddress()), 16)
+                        #print(int(str(instructions[ind].getAddress()), 16))
+                        #print(f"Call before: {instructions[ind].getAddress()}")
+                        break
+    jump_to = 0
+    jump_address = 0
+    for ind, i in enumerate(instructions_readable):
         for j in jump_instructions:
             if i.split(" ")[0].lower()==j:
-                if int(i.split(" ")[1], 16)>=int("0x"+str(addr_set).split(',')[0][2:], 16) and int(i.split(" ")[1], 16)<=int("0x"+str(addr_set).split(' ')[1][1:-1], 16):
+                if (jump_target:=int(i.split(" ")[1], 16))>=int("0x"+str(addr_set).split(',')[0][2:], 16) and int(i.split(" ")[1], 16)<=int("0x"+str(addr_set).split(' ')[1][1:-1], 16):
                     conditionals[1] = True
-                    
-    return [True if conditionals[0] and conditionals[1] else False, 0] #usability not added yet
+                    jump_address = int(str(instructions[ind].getAddress()), 16)
+                    jump_to = jump_target
+
+    if conditionals[0] and conditionals[1] and jump_to!=0 and call_addr!=0 and call_addr>=jump_to and call_addr<jump_address:
+        conditionals[2] = True
+        
+
+    #print(f"Call: {call_addr}")
+    #print(f"Jump: {jump_to}")
+
+    return [True if conditionals[0] and conditionals[1] and conditionals[2] else False, 0] #usability not added yet
+
+
 
 def test_ghidra():
+    if len(sys.argv)<2:
+        print("Invalid arguments\nTry: python {} binary_name_here.exe".format(__file__.split("/")[-1]))
+        exit()
     try:
         with open(f"{sys.argv[1]}", 'r') as file:
             pass
@@ -114,11 +155,26 @@ def test_ghidra():
         with pyhidra.open_program(f"{sys.argv[1]}") as bin:
             pass
     except ValueError:
-        print("Please set your GHIDRA_INSTALL_DIR environment variable to your ghidra installation directory\nHint: If you are on Linux, run \'which ghidra\' to find the installation directrory")
+        print("\nPlease set your GHIDRA_INSTALL_DIR environment variable to your ghidra installation directory\nHint: If you are on Linux, run \'which ghidra\' to find the installation directrory")
         exit()
+    #Get the address of the VTABLE functions, then get the address of the call instruction,
+    #Get the function start address
+
+    #Now check for a jmp
+
+            #Now check for a dereference in a future instruction
+            #Check for proper dereferences
+            #Add a normal register and a multiply arch term that need to be found
+    #call_ind:int = instruction_index(instructions_readable, 'CALL')
+    #print(call_ind)
+    #print(instructions_readable)
+    #Check if register's move instruction has [r1 + arch*r2], if so, check if that register is dereferenced before a call instruction, check if the register that is dereferencing it is being called
+    #So Search for get inside the [] and see if it has 1 register + another register*arch, don't check what it is, only check what it is being moved to, then check if that is being dereferenced just with [], track all dereferences of it to see if the deref is called
+    #
+    #If there is a jump instruction, if it is > than the function base and < than the call, continue
+
 
 def main() -> None:
-    print("This is an experimental version of V++. Currently, this script has limited functionality. Please check the repository for updates.")
     test_ghidra()
     pyhidra.start()
     with pyhidra.open_program(f"{sys.argv[1]}") as bin:
@@ -126,11 +182,23 @@ def main() -> None:
         manager = program.getFunctionManager()
         iterator = manager.getFunctions(True)
         func_list = []
+        print("Finding vfgadgets...")
         while iterator.hasNext():
             func = iterator.next()
+            #print(func.getName())
+            #get_instruction_offset(program, func.getEntryPoint()) #func.getBody())
             instructions:list[str] = list(program.getListing().getInstructions(func.getBody(), 1))
+            #print(func)
+            #print(func.getEntryPoint()) #Search the data section for this
+            #print(type(func.getEntryPoint()))
+            #print(program.getListing().getInstructionAt(func.getEntryPoint())) #Need address range, also only gets code
+            #print(f"FUNC: {func.getBody()}")
+            #print(instructions) #Need address range, also only gets code
             if (is_mlg(instructions, func.getBody())[0]):
-                print(f"Main Loop Gadget found at: {func.getEntryPoint()}")
+                print(f"Potential Main Loop Gadget found at: {func.getEntryPoint()}")
+            #print(program.getListing().getCodeUnits(func.getBody())) #Need address range, also only gets code
+            #print(get_disassembly(program, func))
+        #print(f"PROGRAM: {program}")
 
 if __name__=="__main__":
     main()
